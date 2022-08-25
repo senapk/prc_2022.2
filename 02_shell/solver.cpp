4f92a935f4d3f96a9dc4e1130b242160b83ecc59
David Sena <sena.ufc@gmail.com>
2022-08-25 19:33:27 -0300

    up

--- 02_shell/solver.cpp
+++ 02_shell/solver.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <iomanip>
 
+bool in(std::vector<int> vet, int value) {
+    for (auto elem : vet)
+        if (elem == value)
+            return true;
+    return false;
+}
 
+double average(std::vector<int> vet) {
+    double value {};
+    for (auto elem : vet)
+        value += abs(elem);
+    return value / vet.size();
+}
 
 std::vector<std::string> split(std::string texto, char sep) {
     std::stringstream ss(texto);
@@ -12,7 +25,6 @@ std::vector<std::string> split(std::string texto, char sep) {
         vet.push_back(token);
     return vet;
 }
-
 template <typename T>
 T to(std::string data) {
     T value {};
@@ -20,7 +32,7 @@ T to(std::string data) {
     return value;    
 }
 
-std::vector<int> from_vet(std::string data) {
+std::vector<int> to_vet(std::string data) {
     std::vector<int> vet;
     for (auto elem : split(data.substr(1, data.size() - 2), ','))
         vet.push_back(to<int>(elem));
@@ -36,11 +48,16 @@ std::string fmt(std::vector<int> vet) {
     return ss.str();
 }
 
+std::string fmt(bool value) {
+    return value ? "true" : "false";
+}
+
+std::string fmt(double value) {
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(2) << value;
+    return ss.str();
+}
 int main() {
-    std::cout << fmt(from_vet("[1,2,3]")) << '\n';
-    return 0;
-
-
     while (true) {
         std::string line {};
         std::getline(std::cin, line); //ler a linha
@@ -49,11 +66,11 @@ int main() {
 
         if (cmd == "end") {
             break;
-        } else if (cmd == "soma") {// soma 5 9
-            std::cout << (to<int>(ui[1]) + to<int>(ui[2])) << '\n';
-        } else if (cmd == "soma_all") {// soma_all [1,2,3,7]
-            std::cout << (to<int>(ui[1]) + to<int>(ui[2])) << '\n';
-        } else {
+        } else if (cmd == "in") {// in [1,2,3] 4
+            std::cout << fmt(in(to_vet(ui[1]), to<int>(ui[2]))) << '\n';
+        } else if (cmd == "average") {
+            std::cout << fmt(average(to_vet(ui[1]))) << '\n';
+        } else {            
             std::cout << "fail: comando invalido\n";
         }
     }
